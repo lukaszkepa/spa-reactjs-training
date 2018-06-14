9a41b05c3fb3c98dad04ecbe39690c94bd4eca31
Łukasz Kępa <lukasz.kepa@cybercom.com>
2018-06-14 17:23:12 +0200

    Added bootstrap and configured ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -8,23 +9,52 @@ module.exports = {
         exclude: '/node_modules/',
         use: {
           loader: 'babel-loader',
-        }
+        },
       },
       {
         test: /\.html$/,
         use: {
           loader: 'html-loader',
-        }
-      }
-    ]
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
     }),
-  ]
+    new CopyWebpackPlugin([
+      {
+        from: './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        to: 'css',
+        flatten: true,
+      },
+    ]),
+  ],
 };
